90aa2c4acbee882f530140647ccc1774c4ad3e61
elodiec <elodiecompainfr@gmail.com>
2018-12-19 10:57:09 +0100

    stock par client ok

---
+++ resources/views/client/stocks.blade.php
@@ -0,0 +1,20 @@
+@extends('main')
+
+@section('content')
+    <div class="col-sm-12">
+        <h1>Exemple stocks clients</h1>
+        <table class="table table-striped">
+            <thead class="thead-dark">
+            <th>nom</th>
+            <th>stocks</th>
+            </thead>
+            @foreach($stocks as $stock)
+                <tr>
+                    <td>{{$stock->code}}</td>
+                    <td>{{$stock->total}}</td>
+                </tr>
+            @endforeach
+            {{--{{$stock->links()}}--}}
+        </table>
+    </div>
+    @endsection
